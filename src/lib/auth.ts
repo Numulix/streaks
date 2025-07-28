6eb881f36b0d5ed721959cb11fe92d5ae550d105
Numulix <jbabic999@gmail.com>
2025-07-28 22:10:47 +0200

    feat: configure nextauth with middleware

---
+++ src/lib/auth.ts
@@ -0,0 +1,26 @@
+import { NextAuthOptions } from 'next-auth'
+import { PrismaAdapter } from '@auth/prisma-adapter'
+import { prisma } from '@/lib/prisma'
+import GoogleProvider from 'next-auth/providers/google'
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+    }),
+  ],
+  callbacks: {
+    session: ({ session, token }) => ({
+      ...session,
+      user: {
+        ...session.user,
+        id: token.sub,
+      },
+    }),
+  },
+  pages: {
+    signIn: '/auth/signin',
+  },
+}
